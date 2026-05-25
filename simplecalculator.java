2ba6e6c451f933c9721498b6129a87aa4ba8e9f3
Aakanksha Jare <jareaakanksha@gmail.com>
2026-05-25 13:40:06 +0530

    Add files via upload

---
+++ simplecalculator.java
@@ -0,0 +1,11 @@
+public class simplecalculator {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        System.out.println("multiplication=" + (a*b));
+        System.out.println("addition=" + (a+b));
+        System.out.println("division" + (a/b));
+        System.out.println("subtraction" + (a-b));
+        System.out.println("remainder" +(a%b));
+    }
+}
